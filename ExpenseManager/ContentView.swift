14bbb645474d6e01d5782c5b1c9afa8d5ec0cd13
Carlos Mendoza <carlos.mba19@gmail.com>
2024-07-15 09:10:16 -0700

    Initial Commit

---
+++ ExpenseManager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ExpenseManager
+//
+//  Created by Carlos Mendoza on 15/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ExpenseManager/ExpenseManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseManagerApp.swift
+//  ExpenseManager
+//
+//  Created by Carlos Mendoza on 15/07/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
